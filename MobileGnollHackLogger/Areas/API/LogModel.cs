f34a17cf6e6b98e2673ed898d164cf31da9e99f9
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2023-12-05 20:40:37 +0200

    Removed Email from LogModel.

--- MobileGnollHackLogger/Areas/API/LogModel.cs
+++ MobileGnollHackLogger/Areas/API/LogModel.cs
@@ -18,10 +18,6 @@ namespace MobileGnollHackLogger.Areas.API
         public string? UserName { get; set; }
 
         [Required]
-        [EmailAddress]
-        public string? Email { get; set; }
-
-        [Required]
         public string? Password { get; set; }
 
         [Required]
