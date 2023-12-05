18f3cd96992d9ae61ef12bb43a48084450d5686d
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2023-12-05 19:46:27 +0200

    Deleted comment.

--- MobileGnollHackLogger/Areas/API/LogController.cs
+++ MobileGnollHackLogger/Areas/API/LogController.cs
@@ -6,8 +6,6 @@ using MobileGnollHackLogger.Data;
 using System.Net;
 using System.Text;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace MobileGnollHackLogger.Areas.API
 {
     [Route("xlogfile")]
