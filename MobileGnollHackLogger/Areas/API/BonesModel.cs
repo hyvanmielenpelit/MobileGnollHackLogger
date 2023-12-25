0d1db844e17565c22ed85edf5ce1a160e23b95c3
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2023-12-25 12:47:17 +0200

    Added AllowedUsers to BonesModel.

--- MobileGnollHackLogger/Areas/API/BonesModel.cs
+++ MobileGnollHackLogger/Areas/API/BonesModel.cs
@@ -10,6 +10,7 @@ namespace MobileGnollHackLogger.Areas.API
 
         public string? Command { get; set; }
         public string? Data { get; set; }
+        public string? AllowedUsers { get; set; }
 
         public ulong VersionNumber { get; set; }
         public ulong VersionCompatibilityNumber { get; set; }
