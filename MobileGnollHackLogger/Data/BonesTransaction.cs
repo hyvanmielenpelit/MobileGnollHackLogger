c0918e2b07be08b1fe9124ec83b65814ed8b94da
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2024-01-05 17:34:50 +0200

    Bones Transactions page

--- MobileGnollHackLogger/Data/BonesTransaction.cs
+++ MobileGnollHackLogger/Data/BonesTransaction.cs
@@ -25,7 +25,9 @@ namespace MobileGnollHackLogger.Data
 
         public TransactionType Type { get; set; }
 
+        [ForeignKey("Bones")]
         public long BonesId { get; set; }
+        public Bones? Bones { get; set; }
 
         public int DifficultyLevel { get; set; }
 
---
+++ MobileGnollHackLogger/Pages/BonesTransactions.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MobileGnollHackLogger.Data;
+
+namespace MobileGnollHackLogger.Pages
+{
+    public class BonesTransactionsModel : PageModel
+    {
+        public ApplicationDbContext DbContext { get; set; }
+
+        public string? Title { get; set; }
+
+        public BonesTransactionsModel(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+            Title = "Bones Upload and Download Statistics";
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
