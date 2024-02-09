cee7ff7df8a9c3e53b7c0a75c87560f3dd6e59af
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2024-02-09 11:03:32 +0200

    More preliminary work on ReplayController.

--- MobileGnollHackLogger/Areas/API/ReplayController.cs
+++ MobileGnollHackLogger/Areas/API/ReplayController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MobileGnollHackLogger.Data;
+using System.Globalization;
+using System.Text;
 
 namespace MobileGnollHackLogger.Areas.API
 {
@@ -51,7 +54,136 @@ namespace MobileGnollHackLogger.Areas.API
         [HttpPost]
         public async Task<IActionResult> Post([FromForm] ReplayModel model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _dbLogger.RequestMethod = Request.Method;
+                _dbLogger.LastRequestId = Guid.NewGuid();
+                _dbLogger.RequestPath = Request.GetEncodedUrl();
+                _dbLogger.UserIPAddress = Request.HttpContext.Connection.RemoteIpAddress?.ToString();
+
+                if (model == null)
+                {
+                    int responseCode = 400;
+                    await _dbLogger.LogRequestAsync("model is null", Data.LogLevel.Error, responseCode);
+                    return StatusCode(responseCode); //Bad Request
+                }
+
+                _dbLogger.RequestData = model.GetJson();
+
+                if (model.UserName == null)
+                {
+                    int responseCode = 400;
+                    await _dbLogger.LogRequestAsync("model.UserName is null", Data.LogLevel.Error, responseCode);
+                    return StatusCode(responseCode); //Bad Request
+                }
+
+                _dbLogger.RequestUserName = model.UserName;
+
+                if (model.Password == null)
+                {
+                    int responseCode = 400;
+                    await _dbLogger.LogRequestAsync("model.Password is null", Data.LogLevel.Error, responseCode);
+                    return StatusCode(responseCode); //Bad Request
+                }
+                if (model.AntiForgeryToken == null)
+                {
+                    int responseCode = 400;
+                    await _dbLogger.LogRequestAsync("model.AntiForgeryToken is null", Data.LogLevel.Error, responseCode);
+                    return StatusCode(responseCode); //Bad Request
+                }
+
+                _dbLogger.RequestAntiForgeryToken = model.AntiForgeryToken;
+
+                var antiForgeryToken = _configuration["AntiForgeryToken"];
+                if (antiForgeryToken != model.AntiForgeryToken)
+                {
+                    int responseCode = 401;
+                    await _dbLogger.LogRequestAsync($"AntiForgeryToken is invalid. Request: '{model.AntiForgeryToken}'. Server: '{antiForgeryToken}'.",
+                        Data.LogLevel.Warning, responseCode);
+                    return StatusCode(responseCode); //Not Authorized
+                }
+
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    //Sign in succeeded
+                    _dbLogger.LoginSucceeded = true;
+
+                    //Check next whether the user is not banned
+                    try
+                    {
+                        var user = await _userManager.FindByNameAsync(model.UserName);
+                        if (user != null)
+                        {
+                            if (user.IsBanned == true)
+                            {
+                                int responseCode = 423;
+                                string msg = "User " + model.UserName + " is banned.";
+                                await _dbLogger.LogRequestAsync(msg, Data.LogLevel.Error, responseCode);
+                                Response.StatusCode = responseCode; //Server Error
+                                return Content(msg);
+                            }
+                            else if (user.IsBonesBanned == true)
+                            {
+                                int responseCode = 423;
+                                string msg = "User " + model.UserName + " is not allowed to send bones.";
+                                await _dbLogger.LogRequestAsync(msg, Data.LogLevel.Error, responseCode);
+                                Response.StatusCode = responseCode; //Server Error
+                                return Content(msg);
+                            }
+                        }
+                        else
+                        {
+                            int responseCode = 500;
+                            string msg = "Server error occurred while verifying user: User is null.";
+                            await _dbLogger.LogRequestAsync(msg, Data.LogLevel.Error, responseCode);
+                            Response.StatusCode = responseCode; //Server Error
+                            return Content(msg);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        int responseCode = 500;
+                        string msg = "Server error occurred while verifying user: " + ex.Message;
+                        await _dbLogger.LogRequestAsync(msg, Data.LogLevel.Error, responseCode);
+                        Response.StatusCode = responseCode; //Server Error
+                        return Content(msg);
+                    }
+
+                    //More code goes here
+
+
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    _dbLogger.LoginSucceeded = false;
+                    int responseCode = 412;
+                    await _dbLogger.LogRequestAsync("Login requires two factor authentication. Error.", Data.LogLevel.Error, responseCode);
+                    return StatusCode(responseCode);
+                }
+                if (result.IsLockedOut)
+                {
+                    _dbLogger.LoginSucceeded = false;
+                    int responseCode = 423;
+                    await _dbLogger.LogRequestAsync("User is locked out.", Data.LogLevel.Warning, responseCode);
+                    return StatusCode(responseCode);
+                }
+                else
+                {
+                    _dbLogger.LoginSucceeded = false;
+                    int responseCode = 403;
+                    await _dbLogger.LogRequestAsync($"Login failed for user '{model.UserName}'.", Data.LogLevel.Warning, responseCode);
+                    return StatusCode(responseCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                int responseCode = 500;
+                string message = (ex.InnerException ?? ex).GetType().FullName + ", Message: " + ex.Message;
+                await _dbLogger.LogRequestAsync(message, Data.LogLevel.Error, responseCode);
+                Response.StatusCode = responseCode;
+                return Content(message);
+            }
         }
 
         [HttpDelete("{id}")]
--- MobileGnollHackLogger/Areas/API/ReplayModel.cs
+++ MobileGnollHackLogger/Areas/API/ReplayModel.cs
@@ -1,6 +1,67 @@
-﻿namespace MobileGnollHackLogger.Areas.API
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MobileGnollHackLogger.Areas.API
 {
     public class ReplayModel
     {
+        [JsonIgnore]
+        public IFormFile? ReplayFile { get; set; }
+
+        public string? Data { get; set; }
+
+        public ulong VersionNumber { get; set; }
+        public ulong VersionCompatibilityNumber { get; set; }
+
+        public string? Platform { get; set; }
+        public string? PlatformVersion { get; set; }
+        public string? Port { get; set; }
+        public string? PortVersion { get; set; }
+        public string? PortBuild { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z0-9_]{1,31}$")]
+        [MaxLength(31)]
+        [JsonIgnore]
+        public string? UserName { get; set; }
+
+        [Required]
+        [MaxLength(63)]
+        [JsonIgnore]
+        public string? Password { get; set; }
+
+        [Required]
+        [JsonIgnore]
+        public string? AntiForgeryToken { get; set; }
+
+        public string GetJson()
+        {
+            return System.Text.Json.JsonSerializer.Serialize<ReplayModel>(this);
+        }
+
+        public string GetPlatform()
+        {
+            return Platform == null ? "Unknown" : Platform;
+        }
+
+        public string GetPlatformVersion()
+        {
+            return PlatformVersion == null ? "" : PlatformVersion;
+        }
+
+        public string GetPort()
+        {
+            return Port == null ? "" : Port;
+        }
+
+        public string GetPortVersion()
+        {
+            return PortVersion == null ? "" : PortVersion;
+        }
+
+        public string GetPortBuild()
+        {
+            return PortBuild == null ? "" : PortBuild;
+        }
     }
 }
