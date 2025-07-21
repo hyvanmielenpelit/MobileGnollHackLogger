5482528c4b0fb2c97ccbc5b78a29577097ef0a88
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2025-07-21 13:09:56 +0300

    Remove Index. With current data, there was no noticeable difference.

---
+++ MobileGnollHackLogger/Data/Migrations/20250721094639_RemoveGameLogIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MobileGnollHackLogger.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveGameLogIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_GameLog_ByteStart_ByteEnd_ByteLength",
+                table: "GameLog");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_GameLog_ByteStart_ByteEnd_ByteLength",
+                table: "GameLog",
+                columns: new[] { "ByteStart", "ByteEnd", "ByteLength" });
+        }
+    }
+}
