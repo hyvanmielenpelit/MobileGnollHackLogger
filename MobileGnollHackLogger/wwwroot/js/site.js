0b9e2db0c23fb6aec84930a8e54b7342c624480e
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2026-07-18 18:14:51 +0300

    Added disabling of the submit button to forms.

--- MobileGnollHackLogger/wwwroot/js/site.js
+++ MobileGnollHackLogger/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    $('.disable-button-on-submit').on('submit', function () {
+        if ($(this).valid && !$(this).valid()) {
+            return false;
+        }
+        $(this).find('button[type="submit"]').prop('disabled', true);
+    });
+});
